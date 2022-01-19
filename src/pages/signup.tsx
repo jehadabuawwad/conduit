aa2f7884969ac0e152e897acf6f85cf60511882e
Jehad Abu Awwad <jehadabuawwad@outlook.com>
2022-01-19 17:01:23 +0200

    feat : sign-up page initially styled

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,12 +1,49 @@
 import React from "react";
 import IPage from "../interfaces/pages";
-
-const SignOut: React.FunctionComponent<IPage> = (props) => {
+import "../style/signup.css";
+const SignUp: React.FunctionComponent<IPage> = (props) => {
   return (
-    <div>
-      <h1>Sign Out Page</h1>
+    <div className="grid-container">
+      <div className="grid-col">
+        <div className="grid-row">
+          <h1 className="text-center">Sign Up</h1>
+          <a className="text-register" href="/register">
+            Have an account?
+          </a>
+        </div>
+        <div className="grid-row">
+          <form>
+            <fieldset>
+              <fieldset className="form-group">
+                <input
+                  type="username"
+                  className="form-control"
+                  placeholder="Username"
+                />
+              </fieldset>
+              <fieldset className="form-group">
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Email"
+                />
+              </fieldset>
+              <fieldset className="form-group">
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Password"
+                />
+              </fieldset>
+              <button className="btn" type="submit">
+                Sign in
+              </button>
+            </fieldset>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default SignOut;
+export default SignUp;
