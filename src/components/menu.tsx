a0bd0f3909e377787bbd12bc77fc2e93aa2a8fca
Jehad Abu Awwad <jehadabuawwad@outlook.com>
2022-01-20 16:29:55 +0200

    feat : menu styled and released

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -2,15 +2,27 @@ import * as React from "react";
 
 import { IMenu } from "../interfaces/components";
 import "../style/menu.css";
+import { useSelector, useDispatch, RootStateOrAny } from "react-redux";
 const Menu: React.FunctionComponent<IMenu> = (props) => {
+  const selectedTag = useSelector((state: RootStateOrAny) => state.selectedTag);
+  const handleTagSelection = () => {
+    const element: any = document.getElementById("HomeMenu");
+    element.style.borderBottom = "1px solid rgb(214, 214, 214)";
+    return (
+      <a href="" className="selceted-tag">
+        # {selectedTag}
+      </a>
+    );
+  };
   return (
     <div>
       <div className="feed-toggle">
         <ul className="nav">
           <li className="nav-item">
-            <a href="" className="nav-link">
+            <a id="HomeMenu" href="" className="nav-link">
               Global Feed
             </a>
+            {selectedTag && handleTagSelection()}
           </li>
         </ul>
       </div>
