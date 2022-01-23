cca7ca3d4fe5e010f5ef9131daab8633dbce488e
Jehad Abu Awwad <jehadabuawwad@outlook.com>
2022-01-23 12:37:11 +0200

    refactor : header component extraction

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,66 +1,22 @@
 import * as React from "react";
 import "../style/header.css";
 import { IHeader } from "../interfaces/components";
-import { Link, Redirect } from "react-router-dom";
+import { Redirect } from "react-router-dom";
+import HeaderSection from "./headersection";
 const Header: React.FunctionComponent<IHeader> = (props) => {
   const isLogged = localStorage.getItem("token");
-  const handleSignOut = () => {
-    localStorage.clear();
-    setTimeout(() => {
-      document.location.reload();
-    }, 250);
-  };
   if (!isLogged) {
     return (
       <>
         <Redirect to="/sign-in" />;
-        <header>
-          <Link id="navbar-brand" to="/">
-            conduit
-          </Link>
-          <nav>
-            <div id="links">
-              <Link to="/">Home</Link>
-
-              {isLogged ? (
-                <Link onClick={handleSignOut} to="/sign-in">
-                  Sign Out
-                </Link>
-              ) : (
-                <>
-                  <Link to="/sign-in">Sign In</Link>
-                  <Link to="/sign-up">Sign Up</Link>
-                </>
-              )}
-            </div>
-          </nav>
-        </header>
+        <HeaderSection name="Header Section" />
       </>
     );
   }
   return (
     <>
       <Redirect to="/" />;
-      <header>
-        <Link id="navbar-brand" to="/">
-          conduit
-        </Link>
-        <nav>
-          <div id="links">
-            <Link to="/">Home</Link>
-            {isLogged ? (
-              <Link onClick={handleSignOut} to="/sign-in">
-                Sign Out
-              </Link>
-            ) : (
-              <>
-                <Link to="/sign-in">Sign In</Link>
-                <Link to="/sign-up">Sign Up</Link>
-              </>
-            )}
-          </div>
-        </nav>
-      </header>
+      <HeaderSection name="Header Section" />
     </>
   );
 };
