4b39f477a0d459dff9d5db4cbdb8b49e35083c63
Jehad Abu Awwad <jehadabuawwad@outlook.com>
2022-01-23 16:26:15 +0200

    refactor : header composing

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,38 @@
 import * as React from "react";
 import "../style/header.css";
 import { IHeader } from "../interfaces/components";
-import HeaderSection from "./headersection";
+import { Link } from "react-router-dom";
 const Header: React.FunctionComponent<IHeader> = (props) => {
+  const isLogged = localStorage.getItem("token");
+  const handleSignOut = () => {
+    localStorage.clear();
+    setTimeout(() => {
+      document.location.reload();
+    }, 250);
+  };
   return (
     <>
-      <HeaderSection name="Header Section" />
+      <header>
+        <Link id="navbar-brand" to="/">
+          conduit
+        </Link>
+        <nav>
+          <div id="links">
+            <Link to="/">Home</Link>
+
+            {isLogged ? (
+              <Link onClick={handleSignOut} to="/sign-in">
+                Sign Out
+              </Link>
+            ) : (
+              <>
+                <Link to="/sign-in">Sign In</Link>
+                <Link to="/sign-up">Sign Up</Link>
+              </>
+            )}
+          </div>
+        </nav>
+      </header>
     </>
   );
 };
